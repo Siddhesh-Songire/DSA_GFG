b6e994e7b91e586f481e90c06d3017449989dd5c
Siddhesh <siddhehsongire@gmail.com>
2022-06-20 17:25:35 +0530

    Majority element

---
+++ Prob_40.cpp
@@ -0,0 +1,27 @@
+/*
+*169. Majority Element
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+! class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size()/2;
+        return nums[n];
+    }
+};
+! Moors vooting algo tha par samja nahi
+*/
